28b56dc560cce1598ccc70a94c087f5635fa767b
mactodd315 <mactodd315@Ubuntu22.10.myguest.virtualbox.org>
2023-06-14 15:48:12 -0400

    Update files from local

--- ks_grav_test.py
+++ ks_grav_test.py
@@ -32,11 +32,7 @@ def generate_posterior(prior, num_simulations = 1000, num_workers = 1):
 def get_counts(posterior, observation, bounds,n_pixels):
     observation = torch.as_tensor(observation)
     posterior_samples = np.asanyarray(posterior.sample((10000,), x = observation, show_progress_bars = False))
-#     inclination_samples = [sample[0] for sample in posterior_samples]
-    distance_samples = [sample[1] for sample in posterior_samples]
-    print(distance_samples)
-    counts, bins = np.histogram(distance_samples, bins = n_pixels, range=(bounds[0],bounds[1]), density = True)
-    print(counts)
+    counts, bins = np.histogram(posterior_samples, bins = n_pixels, range=(bounds[0],bounds[1]), density = True)
     return counts
 
 def true_in_interval(interval_width, theta, counts, true_parameter):
@@ -50,7 +46,6 @@ def true_in_interval(interval_width, theta, counts, true_parameter):
 
 
 if __name__ == "__main__":
-#     n_observations = 1 
     num_simulations = 10000
     num_workers = 1
     n_pixels = 50
@@ -60,7 +55,7 @@ if __name__ == "__main__":
     #1-d problem
     # inclination_bounds = [torch.tensor([0]), torch.tensor([torch.pi/2])]
     # distance_bounds = [torch.tensor([.1]), torch.tensor([50.0])]
-    # bounds = distance_bounds
+    # bounds = inclination_bounds
     # prior = utils.BoxUniform(low = bounds[0]*torch.ones(1), high = bounds[1]*torch.ones(1))
     
     #2-d problem
@@ -71,83 +66,62 @@ if __name__ == "__main__":
     sbi_posterior = generate_posterior(prior, num_simulations, num_workers)
     print("Posterior Trained.")
 
-    observation, true_parameter = sample_observations()
-    print(true_parameter)
-    posterior_samples = sbi_posterior.sample((10000,), x = torch.as_tensor(observation))
-    _ = analysis.pairplot(posterior_samples, limits = [[0,1.6], [.1,50]], figsize = (5,5))
-    plt.show()
-    
+        
     #generate trues interval array (y-axis of pp test)
-    n_intervals = 10
+    n_intervals = 100
     credible_intervals = np.linspace(0,1,n_intervals)
     fraction_true_in_interval = np.zeros(n_intervals)
+    samples = 100
     
-    #distance parameter
-    print("Sampling Distance parameters.")
-    bounds = [0.1,50.0]
-    theta =  np.linspace(bounds[0],bounds[1],n_pixels)
-    pixelwidth = (bounds[1]-bounds[0])/n_pixels
-    for i in range(n_intervals-1):
-        ci = credible_intervals[i]
-        samples = 0
-        print("Evaluating credible interval: ", ci)
-        while samples<2:
-            print(samples)
-            samples+=1
-            observation, true_parameter = sample_observations()
-            counts = get_counts(sbi_posterior, observation, bounds, n_pixels)*pixelwidth
-            print(true_parameter)
-            if true_in_interval(ci, theta, counts, true_parameter[1]):
-                break
-        fraction_true_in_interval[i] = 1/samples
-    print(fraction_true_in_interval)
-    Result = kstest(fraction_true_in_interval, "uniform")
-    print(Result.pvalue)
+    sample_parameter = input("Parameter to Vary: ")
+    
+    if sample_parameter == 'distance':
+        #distance parameter
+        print("Sampling Distance parameters.")
+        observations, true_parameters = sample_observations(length = samples,filename='observations_vdistance.hdf5')
+        bounds = [0.1,50.0]
+        theta =  np.linspace(bounds[0],bounds[1],n_pixels)
+        pixelwidth = (bounds[1]-bounds[0])/n_pixels
+        for i in range(n_intervals-1):
+            ci = credible_intervals[i]
+            print("Evaluating credible interval: ", ci)
+            trues = 0
+            for j in range(samples):
+                counts = get_counts(sbi_posterior, observations[j], bounds, n_pixels=n_pixels)*pixelwidth
+                if true_in_interval(ci, theta, counts, true_parameters[j][1]):
+                    trues+=1
+            fraction_true_in_interval[i] = trues/samples
+    
+    elif sample_parameter == 'inclination':
+        #inclination parameter
+        print("Sampling Inclination parameters.")
+        observations, true_parameters = sample_observations(length = samples,filename='observations_vinclination.hdf5')
+        bounds = [0,np.pi/2]
+        theta =  np.linspace(bounds[0],bounds[1],n_pixels)
+        pixelwidth = (bounds[1]-bounds[0])/n_pixels
+        for i in range(n_intervals-1):
+            ci = credible_intervals[i]
+            print("Evaluating credible interval: ", ci)
+            trues = 0
+            for j in range(samples):
+                counts = get_counts(sbi_posterior, observations[j], bounds, n_pixels=n_pixels)*pixelwidth
+                if true_in_interval(ci, theta, counts, true_parameters[j][0]):
+                    trues+=1
+            fraction_true_in_interval[i] = trues/samples
+    else:
+        print("Not a parameter.")
+    fraction_true_in_interval[-1] = 1
 
     f = h5py.File("ks_test_results.hdf5", "a")
 
-    grp = f.creat_group("pp_tests")
+    grp = f.create_group("pp_tests")
+    dset = grp.create_dataset("FTI"+sample_parameter+str(num_simulations), data = fraction_true_in_interval)
+    dset.attrs["N_Simulations"] = num_simulations
+    dset.attrs["Sample Number"] = samples
+    dset.attrs["N of Intervals"] = n_intervals
 
 
      
-    #inclination parameter
-#     bounds = [0.0,np.pi/2]
-#     theta =  np.linspace(bounds[0],bounds[1],n_pixels)
-#     pixelwidth = (bounds[1]-bounds[0])/n_pixels
-#     for i in range(n_intervals-1):
-#         ci = credible_intervals[i]
-#         samples = 0
-#         while True:
-#             samples+=1
-#             observation, true_parameter = sample_observations()
-#             counts = get_counts(sbi_posterior, observation, bounds, n_pixels)*pixelwidth
-#             if true_in_interval(ci, theta, counts, true_parameter[0]):
-#                 break
-#         necessary_observations[i] = 1/samples
-#     print(necessary_observations)
-    
-#     observations, true_parameters = sample_observations(n_observations)
-#     theta = np.linspace(bounds[0],bounds[1],n_pixels)
-#     pixelwidth = (bounds[1].item()-bounds[0].item())/n_pixels
-#     counts = [get_counts(sbi_posterior, each, n_observations, bounds,n_pixels)*pixelwidth for each in observations]
-#     for i in range(n_intervals-1):
-#         ci = credible_intervals[i]
-#         trues = 0
-#         for j in range(n_observations):
-#             true_parameter = true_parameters[j][1].item()
-#             if true_in_interval(ci, theta, counts[j], true_parameter):
-#                 trues += 1
-#         trues_in_intervals[i] = trues/n_observations
-#     trues_in_intervals[-1] = 1
-    
-    #plots ideal versus results of pp_test for calculated posterior    
-    
-#     Result = kstest(trues_in_intervals, "uniform")
-#     print(Result)
-#     plt.plot(Result.pvalue())
-#     plt.xlabel("Credible Interval")
-#     plt.ylabel("pvalue")
-#     plt.grid()
-#     plt.savefig("kstest.png")
+
     
     
--- observations.py
+++ observations.py
@@ -4,38 +4,42 @@ from pycbc.waveform import get_td_waveform
 from noise import sample_noise
 import random
 
-def sample_observations(length=1):
+def sample_observations(length=1, filename='observations.hdf5'):
     # samples observations from a pre-generated noise file and returns it as a tensor of inputed length
-    f2 = h5py.File('observations.hdf5','r')
+    f2 = h5py.File(filename,'r')
     grp = f2["waveforms"]
-    parameters = []
     list_of_keys = []
+    list_of_waveforms = []
+    list_of_parameters = []
     for each in grp.keys():
         list_of_keys.append(each)
-    key = random.choice(list_of_keys)
-    dset = grp[key]
-    parameters = dset.name
-    parameters = parameters[11:].split(',')
-    parameters = [float(each) for each in parameters]
-    waveform = dset[:,]
+    for i in range(length):
+        key = random.choice(list_of_keys)
+        dset = grp[key]
+        parameters = dset.name
+        parameters = parameters[11:].split(',')
+        parameters = [float(each) for each in parameters]
+        waveform = dset[:,]
+        list_of_waveforms.append(waveform)
+        list_of_parameters.append(parameters)
        
-    return waveform, parameters
+    return list_of_waveforms, list_of_parameters
 
 
 def generate_observations(number = 1000):
-    f2 = h5py.File("observations_vinclination.hdf5", "w")
+    f2 = h5py.File("observations_vdistance.hdf5", "w")
 #     f = open("observations_vdistance.txt", "w")
 #     f1 = open("observation_parameters_vdistance.txt", "w")
     
     waveforms = f2.create_group("waveforms")
-    inclinations = np.linspace(0, 1.5, 10000)
-    distances = np.linspace(.1, 50, 10000)
+    inclinations = np.linspace(0, 1.5, number)
+    distances = np.linspace(.1, 50, number)
 
     for i in range(number):
 #         inclination = torch.as_tensor(np.random.choice(inclinations))
 #         distance = torch.tensor([22.0])
-        distance = 1.0
-        inclination = np.random.choice(inclinations)
+        distance = distances[i]
+        inclination = 1.0
         hp, hc = get_td_waveform(approximant = 'SEOBNRv4', mass1= 200, mass2 = 200,delta_t = 1/4096, f_lower=30)
         noise = sample_noise(len(hp))
         waveform = (noise+hp*.5*(1+np.cos(inclination)**2)/distance)*10**18
