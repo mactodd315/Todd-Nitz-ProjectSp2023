9236737c3aeda7bf80101bcb037b323cf2553385
mactodd315 <mactodd315@gmail.com>
2023-06-23 10:27:22 -0400

    updating files from cluster

--- cuda_sbi_test.py
+++ cuda_sbi_test.py
@@ -1,6 +1,8 @@
 
 import torch
 import numpy as np
+import matplotlib.pyplot as plt
+import h5py
 
 from sbi.inference import SNPE, prepare_for_sbi, simulate_for_sbi
 from sbi import utils
@@ -26,13 +28,18 @@ def generate_posterior(prior, num_simulations = 1000, num_workers = 1):
     
     simulator, prior = prepare_for_sbi(td_approx, prior)
     inference = SNPE(prior, device = 'cuda')
-    theta, x = simulate_for_sbi(simulator, proposal=prior, num_simulations=1000)
+    theta, x = simulate_for_sbi(simulator, proposal=prior, num_simulations=num_simulations)
     density_estimator = inference.append_simulations(theta,x).train()
     posterior = inference.build_posterior(density_estimator)
     return(posterior)
 
-sbi = generate_posterior(prior)
+num_simulations = 1000
+sbi = generate_posterior(prior,num_simulations)
 observations, parameters = sample_observations(3, filename='datafiles/observations.hdf5', source = 'cluster')
 samples = sbi.sample((10000,), x=torch.as_tensor(observations[0],device = 'cuda'))
 samples = np.asanyarray(samples.cpu())
-print(samples)
+f=h5py.File("temp_2d_samples.hdf5", 'a')
+grp = f.require_group("2d_samples")
+dset = grp.create_dataset("samples"+str(num_simulations), data=samples)
+dset.attrs["true_parameter"] = parameters[0]
+f.close()
